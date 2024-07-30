7a92b259dbc601f1e9e9217eac7d87766e2bcc1f
Joshua Davian <joshuadavian@gmail.com>
2024-07-30 19:59:23 +0700

    feat: Package & Package Question Model & Migration

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -18,5 +19,10 @@ class Question extends Model
     public function questionOptions(): HasMany
     {
         return $this->hasMany(QuestionOption::class);
+    }
+
+    public function packages(): BelongsToMany
+    {
+        return $this->belongsToMany(Package::class);
     }
 }
