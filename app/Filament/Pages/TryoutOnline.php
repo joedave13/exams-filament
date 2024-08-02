95708da00c45811a6caf4a4022bc10b6065f0dab
Joshua Davian <joshuadavian@gmail.com>
2024-08-02 23:10:08 +0700

    feat: Tryout Online Filament Page

---
+++ app/Filament/Pages/TryoutOnline.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class TryoutOnline extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.tryout-online';
+}
